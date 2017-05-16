fe14779484b610ad50e7f5eeeb804bf275d7ef7b
Jayanth <jayanth.rordev@gmail.com>
2017-05-16 12:15:01 -0400

    refs#15494 Updating enviromental variable

--- app/data_migrations/update_invalid_benefit_group_assignment_for_census_employee.rb
+++ app/data_migrations/update_invalid_benefit_group_assignment_for_census_employee.rb
@@ -2,16 +2,19 @@ require File.join(Rails.root, "lib/mongoid_migration_task")
 
 class UpdateInvalidBenefitGroupAssignmentForCensusEmployee < MongoidMigrationTask
   def migrate
-    id = ENV["employee_role_id"].to_s
-    employee_role = EmployeeRole.find(id)
-    employee_role.census_employee.benefit_group_assignments.each do |bga|
-      if bga.is_active? && !bga.valid?
-        bga.update_attribute(:aasm_state, 'coverage_expired')
-        bga.update_attribute(:is_active, false)
-        bga.update_attribute(:hbx_enrollment_id, nil)
-        puts "Updated Benefit group assignment for Employee with id: #{employee_role_id}" unless Rails.env.test?
-      end
-    end   
+    begin
+      census_employee = CensusEmployee.where(_id: ENV["census_employee_id"].to_s).first
+      census_employee.benefit_group_assignments.each do |bga|
+        if bga.is_active? && !bga.valid?
+          bga.update_attribute(:aasm_state, 'coverage_expired')
+          bga.update_attribute(:is_active, false)
+          bga.update_attribute(:hbx_enrollment_id, nil)
+          puts "Updated Benefit group assignment for Employee with id: #{employee_role_id}" unless Rails.env.test?
+        end
+      end   
+    rescue => e
+      puts "Exception: #{e}, CensusEmployee: #{ENV['census_employee_id']}" unless Rails.env.test? 
+    end 
   end
 end
 
--- spec/data_migrations/update_invalid_benefit_group_assignment_for_census_employee_spec.rb
+++ spec/data_migrations/update_invalid_benefit_group_assignment_for_census_employee_spec.rb
@@ -16,26 +16,20 @@ describe UpdateInvalidBenefitGroupAssignmentForCensusEmployee do
 
     let(:family) { FactoryGirl.create(:family, :with_primary_family_member)}
     let!(:hbx_enrollment1) { FactoryGirl.create(:hbx_enrollment, household: family.active_household, benefit_group_assignment_id: benefit_group_assignment1.id, benefit_group_id: benefit_group.id)}
-    let!(:hbx_enrollment2) { FactoryGirl.create(:hbx_enrollment, household: family.active_household, benefit_group_assignment_id: benefit_group_assignment2.id, benefit_group_id: benefit_group_two.id)}
     let(:census_employee) { FactoryGirl.create(:census_employee)}
-    let!(:employee_role) { FactoryGirl.create(:employee_role, census_employee: census_employee)}
     let!(:benefit_group_assignment1)  { FactoryGirl.create(:benefit_group_assignment, census_employee: census_employee) }
-    let!(:benefit_group_assignment2)  { FactoryGirl.create(:benefit_group_assignment, benefit_group: benefit_group_two, census_employee: census_employee) }
     let(:benefit_group) { FactoryGirl.create(:benefit_group, plan_year: plan_year, title: "this is our title") }
-    let(:benefit_group_two) { FactoryGirl.create(:benefit_group, plan_year: plan_year) }
     let(:plan_year)         { FactoryGirl.create(:plan_year, employer_profile: employer_profile) }
     let(:employer_profile)  { FactoryGirl.create(:employer_profile) }
 
     before(:each) do
-      allow(ENV).to receive(:[]).with("employee_role_id").and_return(employee_role.id.to_s)
-      allow(employee_role).to receive(:census_employee).and_return(census_employee)
+      allow(ENV).to receive(:[]).with("census_employee_id").and_return(census_employee.id)
       benefit_group_assignment1.hbx_enrollment_id = hbx_enrollment1.id
       benefit_group_assignment1.save
     end
 
    
     it "should update benefit group assignment for census employee" do
-      allow(employee_role).to receive_message_chain(:census_employee, :benefit_group_assignments).and_return([benefit_group_assignment1, benefit_group_assignment2])
       subject.migrate
       benefit_group_assignment1.reload
       expect(benefit_group_assignment1.hbx_enrollment_id).to be_nil
